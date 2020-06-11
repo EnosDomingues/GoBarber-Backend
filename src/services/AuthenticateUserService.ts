2b9126c262ce3b8a3b30c05912903cbd195a3099
Enos Domingues <enosdomingues@gmail.com>
2020-06-10 21:50:15 -0300

    Gerando token JWT

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,5 +1,6 @@
 import { getRepository } from 'typeorm';
 import { compare } from 'bcryptjs';
+import { sign } from 'jsonwebtoken';
 
 import User from '../models/User';
 
@@ -10,6 +11,7 @@ interface Request {
 
 interface Response {
   user: User;
+  token: string;
 }
 
 class AuthenticateUserService {
@@ -32,9 +34,12 @@ class AuthenticateUserService {
       throw new Error('Incorrect email/password combination.');
     }
 
-    // Usuário autenticado
+    const token = sign({}, '45b4483d060b3ccca93a9b6c113db0a4', {
+      subject: user.id,
+      expiresIn: '1d',
+    });
 
-    return { user };
+    return { user, token };
   }
 }
 
