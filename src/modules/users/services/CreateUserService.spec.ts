87b400d540b36e71a36d23be61f2bfd1af4871d9
Enos Domingues <enosdomingues@gmail.com>
2020-07-02 08:39:15 -0300

    Testando criação de usuário

---
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -0,0 +1,37 @@
+import FakeUsersRepository from '@modules/users/repositories/fakes/FakeUsersRepository';
+import AppError from '@shared/errors/AppError';
+import CreateUserService from './CreateUserService';
+
+describe('CreateUser', () => {
+  it('Should be able to create a new user', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    const user = await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123123',
+    });
+
+    expect(user).toHaveProperty('id');
+  });
+
+  it('Should not be able to create a new user with existing email', async () => {
+    const fakeUsersRepository = new FakeUsersRepository();
+    const createUser = new CreateUserService(fakeUsersRepository);
+
+    await createUser.execute({
+      name: 'John Doe',
+      email: 'johndoe@example.com',
+      password: '123123',
+    });
+
+    expect(
+      createUser.execute({
+        name: 'John Doe',
+        email: 'johndoe@example.com',
+        password: '123123',
+      }),
+    ).rejects.toBeInstanceOf(AppError);
+  });
+});
