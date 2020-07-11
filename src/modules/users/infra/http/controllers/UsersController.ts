0a7aed5ff496c7a47ae599d04ff2b801705da2d0
Enos Domingues <enosdomingues@gmail.com>
2020-07-11 19:33:02 -0300

    Utilizando Class Transformer

--- src/modules/users/infra/http/controllers/UsersController.ts
+++ src/modules/users/infra/http/controllers/UsersController.ts
@@ -1,5 +1,6 @@
 import { Request, Response } from 'express';
 import { container } from 'tsyringe';
+import { classToClass } from 'class-transformer';
 
 import CreateUserService from '@modules/users/services/CreateUserService';
 
@@ -9,12 +10,12 @@ export default class UsersController {
 
     const createUser = container.resolve(CreateUserService);
 
-    await createUser.execute({
+    const user = await createUser.execute({
       name,
       email,
       password,
     });
 
-    return response.json({ name, email });
+    return response.json(classToClass(user));
   }
 }
